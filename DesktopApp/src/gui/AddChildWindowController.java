b857afe766ef27de4d8cd3a2b8c8e46503bb8ecb
Slavko Muzdeka <slavko.muzdeka@student.etf.unibl.org>
2022-12-01 23:45:23 +0100

    Dodane dvije pomocne funckije

--- DesktopApp/src/gui/AddChildWindowController.java
+++ DesktopApp/src/gui/AddChildWindowController.java
@@ -1,13 +1,9 @@
 package gui;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -31,7 +27,7 @@ public class AddChildWindowController {
     private Button btnCreateChildAccount;
 
     @FXML
-    private DatePicker DateOfBirth;
+    private DatePicker dateOfBirth;
 
     @FXML
     private HBox hBoxHorizontalEducators;
@@ -91,11 +87,11 @@ public class AddChildWindowController {
 		file.setTitle("Open File");
 		file.showOpenDialog(stage);
 	}
+	
 	@FXML
 	void btnCreateChildAccountClick(ActionEvent event) {
-		 
 		if(textFieldName.getText() != null && textFieldSurname.getText() != null && textFieldUID.getText() != null && textFieldFatherName.getText() != null &&
-				textFieldMotherName.getText() != null && DateOfBirth.getValue() != null && textFieldFatherPhoneNumber.getText() != null &&
+				textFieldMotherName.getText() != null && dateOfBirth.getValue() != null && textFieldFatherPhoneNumber.getText() != null &&
 				textFieldMotherPhoneNumber.getText() != null && textFieldHeight.getText() != null && textFieldWeight.getText() != null
 				&& textFieldCity.getText() != null && textFieldStreet.getText() != null && textFieldNumber.getText() != null) {
 			
@@ -105,21 +101,50 @@ public class AddChildWindowController {
 			child.setUid(textFieldUID.getText());
 			child.setFatherName(textFieldFatherName.getText());
 			child.setMotherName(textFieldMotherName.getText());
-			child.setDateOfBirth(DateOfBirth.getValue().toString());
+			child.setDateOfBirth(dateOfBirth.getValue().toString());
 			child.setFatherPhoneNumber(textFieldFatherPhoneNumber.getText());
 			child.setMotherPhoneNumber(textFieldMotherPhoneNumber.getText());
 			child.setHeight(textFieldHeight.getText());
 			child.setWeight(textFieldWeight.getText());
-			Address address = new Address(textFieldCity.getText(), textFieldStreet.getText(), textFieldNumber.getText());
-			child.setAddress(address);
-			Note note = new Note(textAreaRemark.getText());
-			child.setNote(note);
+			child.setAddress(new Address(textFieldCity.getText(), textFieldStreet.getText(), textFieldNumber.getText()));
+			child.setNote(new Note(textAreaRemark.getText()));
+			
 			//pozivamo metodu iz service
 			ChildService.addChild(child);
+			clearFields();
 		}else {
-			System.out.println("Nije moguce kreiranje");
-			//kreirati funkciju koja ce prikazivati dijalog da dijete nije moguce kreirati
+			showAlert();
 		}
+	}
+	
+	/**
+	 *  Pomocna funkcija koja prikazuje dijalog da vrijednost svih polja moraju biti popunjenja
+	 *  da bi se nalog djeteta mogao kreirati
+	 */
+	private void showAlert() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Pokušajte ponovo");
+		alert.setHeaderText(null);
+		alert.setContentText("Sva polja moraju biti popunjena.");
+		alert.show();
+	}
+	
+	/**
+	 * Pomocna funckija koja resetuje vrijednost svih polja u slucaju da je nalog djeteta uspjesno kreiran
+	 */
+	private void clearFields() {
+		textFieldName.setText("");
+		textFieldSurname.setText("");
+		textFieldUID.setText("");
+		textFieldFatherName.setText("");
+		textFieldMotherName.setText("");
+		textFieldCity.setText("");
+		textFieldStreet.setText("");
+		textFieldNumber.setText("");
+		textFieldFatherPhoneNumber.setText("");
+		textFieldMotherPhoneNumber.setText("");
+		textFieldHeight.setText("");
+		textFieldWeight.setText("");
 	}
 	
 }
