2fa9292a1aa159f911284ed36b3c682e5b1e2129
Slavko Muzdeka <slavko.muzdeka@student.etf.unibl.org>
2022-12-04 22:59:25 +0100

    Izmijenjen pogled za prikaz djece. Dodana samo jedna tabela za id djeteta, da se zna koji je id za svako dijete

--- BP/Trigeri-Procedure-Pogledi/PrikazDjece-Pogled.sql
+++ BP/Trigeri-Procedure-Pogledi/PrikazDjece-Pogled.sql
@@ -1,5 +1,5 @@
 create view prikaz_djece as
-	select JMB,Ime,Prezime,DatumRodjenja,Ulica,Grad,Broj,Visina,Tezina,ImeOca,ImeMajke,BrojTelefonaOca,BrojTelefonaMajke
+	select OSOBA_IdOsobe, JMB,Ime,Prezime,DatumRodjenja,Ulica,Grad,Broj,Visina,Tezina,ImeOca,ImeMajke,BrojTelefonaOca,BrojTelefonaMajke
 	from dijete d
 	inner join osoba o on o.IdOsobe = d.OSOBA_IdOsobe
-	inner join adresa a on a.IdAdrese = o.ADRESA_IdAdrese
+	inner join adresa a on a.IdAdrese = o.ADRESA_IdAdrese;
